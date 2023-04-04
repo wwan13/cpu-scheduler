3a9adfada64508e2464b0a775244b58e38f928ed
wwan13 <wwan13@naver.com>
2023-04-04 16:07:30 +0900

    [init]: initialize project

---
+++ src/main/java/com/wwan13/cpuscheduler/CpuSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.wwan13.cpuscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpuSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpuSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wwan13/cpuscheduler/CpuSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwan13.cpuscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpuSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
