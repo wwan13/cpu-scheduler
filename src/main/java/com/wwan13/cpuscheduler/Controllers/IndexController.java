b72ef28660f6f0a8010601ebbed3a4ce179dabfa
wwan13 <wwan13@naver.com>
2023-04-21 16:18:57 +0900

    [fix]: index page

--- src/main/java/com/wwan13/cpuscheduler/Controllers/IndexController.java
+++ src/main/java/com/wwan13/cpuscheduler/Controllers/IndexController.java
@@ -2,17 +2,17 @@ package com.wwan13.cpuscheduler.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-
+/***
+ * IndexController Class
+ * Client 의 요청을 처리하는 클래스
+ */
 @Controller
-@RequestMapping(value = "/")
 public class IndexController {
 
-    @GetMapping
-    public String indexPage(HttpServletRequest request) {
-        return "index";
+    @GetMapping("/")
+    public String scheduler() {
+        return "scheduler";
     }
 
 }
--- src/main/java/com/wwan13/cpuscheduler/CpuSchedulerApplication.java
+++ src/main/java/com/wwan13/cpuscheduler/CpuSchedulerApplication.java
@@ -3,6 +3,7 @@ package com.wwan13.cpuscheduler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class CpuSchedulerApplication {
