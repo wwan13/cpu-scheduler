e9a64e648970ca9875764db9bbbfc0a08a82bb2d
wwan13 <wwan13@naver.com>
2023-06-07 22:54:59 +0900

    [feat]: result table insert data

--- frontend/index.js
+++ frontend/index.js
@@ -100,4 +100,106 @@ function init() {
     toHomeButtton.addEventListener("click", changeContents_resultToInput);
 }
 
+const dummyData = {
+    "algorithmType" : "FCFS",
+    "processes" : [
+        {
+            "PID" : "P0",
+            "WT" : 3,
+            "TT" : 7,
+            "RT" : 4
+        },
+        {
+            "PID" : "P1",
+            "WT" : 3,
+            "TT" : 7,
+            "RT" : 4
+        },
+        {
+            "PID" : "P2",
+            "WT" : 3,
+            "TT" : 7,
+            "RT" : 4
+        },
+        {
+            "PID" : "P3",
+            "WT" : 3,
+            "TT" : 7,
+            "RT" : 4
+        },
+    ],
+    "scheduledDataList" : [
+        {
+            "process" : {
+                "PID" : "P0",
+                "WT" : 3,
+                "TT" : 7,
+                "RT" : 4
+            },
+                "startAt" : 0,
+                "endAt" : 3
+        },
+        {
+            "process" : {
+                "PID" : "P0",
+                "WT" : 3,
+                "TT" : 7,
+                "RT" : 4
+            },
+            "startAt" : 4,
+            "endAt" : 7
+        },
+        {
+            "process" : {
+                "PID" : "P0",
+                "WT" : 3,
+                "TT" : 7,
+                "RT" : 4
+            },
+            "startAt" : 8,
+            "endAt" : 10
+        },
+        {
+            "process" : {
+                "PID" : "P0",
+                "WT" : 3,
+                "TT" : 7,
+                "RT" : 4
+            },
+            "startAt" : 11,
+            "endAt" : 16
+        },
+    ],
+    "AWT" : "3",
+    "ATT" : "5"
+}
+
+function setResultTable() {
+    var tableBody = document.getElementsByClassName("table-body")[0];
+    var firstChild = tableBody.children[0]
+    var elseChild = tableBody.children[1].cloneNode(true)
+
+    firstChild.children[0].innerHTML = dummyData.processes[0].PID
+    firstChild.children[1].innerHTML = dummyData.processes[0].WT
+    firstChild.children[2].innerHTML = dummyData.processes[0].TT
+    firstChild.children[3].innerHTML = dummyData.processes[0].RT
+    firstChild.children[4].rowSpan = dummyData.processes.length
+    firstChild.children[5].rowSpan = dummyData.processes.length
+    firstChild.children[4].innerHTML = dummyData.AWT
+    firstChild.children[5].innerHTML = dummyData.ATT
+    tableBody.children[1].remove()
+
+    for (var i=1; i<dummyData.processes.length; i++) {
+        var newNode = elseChild.cloneNode(true);
+        newNode.children[0].innerHTML = dummyData.processes[i].PID
+        newNode.children[1].innerHTML = dummyData.processes[i].WT
+        newNode.children[2].innerHTML = dummyData.processes[i].TT
+        newNode.children[3].innerHTML = dummyData.processes[i].RT
+        tableBody.append(newNode);
+    }
+
+
+}
+
+setResultTable()
 init();
