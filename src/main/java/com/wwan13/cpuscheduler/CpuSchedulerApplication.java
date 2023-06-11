4cfad72f32ee5797cc01bfd5ac0116574ea6b67f
wwan13 <wwan13@naver.com>
2023-06-11 16:39:00 +0900

    [feat]: add description for start project

--- src/main/java/com/wwan13/cpuscheduler/CpuSchedulerApplication.java
+++ src/main/java/com/wwan13/cpuscheduler/CpuSchedulerApplication.java
@@ -10,6 +10,13 @@ public class CpuSchedulerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CpuSchedulerApplication.class, args);
+		System.out.println();
+		System.out.println("--------[프로젝트 실행 방법]--------");
+		System.out.println("1. 브라우저 (크롬, 엣지, 사파리 등) 접속");
+		System.out.println("2. 주소창에 localhost:8080 입력 후 이동");
+		System.out.println("-------------------------------");
+		System.out.println();
+
 	}
 
 }
